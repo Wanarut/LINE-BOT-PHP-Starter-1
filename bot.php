495767885353d1b9b7bf3d0933b04eff81dfe15b
Wanarut Boonyung <wanarut.b@gmail.com>
2019-03-08 17:20:09 +0700

    accept notify

--- bot.php
+++ bot.php
@@ -11,17 +11,17 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
  
-if($arrJson['events'][0]['message']['text'] == "รับแจ้งเตือน"){
+if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "สวัสดี ID ".$arrJson['events'][0]['source']['userId'];
   $arrPostData['messages'][1]['text'] = "รับแจ้งเตือนแล้วครับ";
-}else if($arrJson['events'][0]['message']['text'] == "ดูรูปต้นไม้"){
+}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "image";
-  $arrPostData['messages'][0]['originalContentUrl'] = "http://infarm.oasys-lab.com/scripts/jsonfile/current_image.jpg";
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
 }else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
